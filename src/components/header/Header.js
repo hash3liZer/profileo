19e47684f3bae11d399331dbbcf7a33578eb567f
Shameer Kashif (Shiri) <sheikhshameerkashif@protonmail.com>
2023-06-27 20:10:39 +0500

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -61,7 +61,7 @@ function Header() {
           )}
           {viewBlog && (
             <li>
-              <a href="#blogs">Blogs</a>
+              <a href="https://blog.shameerkashif.me">Blogs</a>
             </li>
           )}
           {viewTalks && (
