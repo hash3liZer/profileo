d52e5fe03f8041ab138ba580f6380cef068e341d
Shameer Kashif (Shiri) <sheikhshameerkashif@protonmail.com>
2023-06-28 12:08:08 +0500

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -391,7 +391,7 @@ const contactInfo = {
 
 const twitterDetails = {
   userName: "hash3liZer", //Replace "twitter" with your twitter username without @
-  display: false // Set true to display this section, defaults to false
+  display: true // Set true to display this section, defaults to false
 };
 
 const isHireable = true; // Set false if you are not looking for a job. Also isHireable will be display as Open for opportunities: Yes/No in the GitHub footer
